12e15ea9b930fc9e0f1e329706afebd9bcd6aa1a
SimJunSik <wnstlr24@naver.com>
2019-04-04 18:27:38 +0900

    0404 set postgre database

--- midgo/settings.py
+++ midgo/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'midgo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'midgodb',
+        'HOST': 'midgo-db.cvyej8u3xdxm.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+        'USER': 'junsik',
+        'PASSWORD': 'zapzap12'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
